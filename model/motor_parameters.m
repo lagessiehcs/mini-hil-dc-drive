98ec93fc9683b4d213f643b34b519c8394cae3d1
Quan Pham <lagessiehcs.git@gmail.com>
2026-09-18 19:39:11 +0200

    Update DC motor model configuration

--- model/motor_parameters.m
+++ model/motor_parameters.m
@@ -1,22 +1,25 @@
 %% DC Motor Parameters
 % Mini HIL DC Motor Project
 
-% Electrical parameters
+%% Electrical parameters
 R  = 2.0;       % Armature resistance [Ohm]
 L  = 0.005;     % Armature inductance [H]
 
-% Motor constants
+%% Motor constants
 Kt = 0.05;      % Torque constant [N*m/A]
 Ke = 0.05;      % Back-EMF constant [V*s/rad]
 
-% Mechanical parameters
+%% Mechanical parameters
 J  = 1e-4;      % Rotor inertia [kg*m^2]
 b  = 1e-4;      % Viscous friction [N*m*s/rad]
 
-% Supply
-V_supply = 6.0; % Motor supply voltage [V]
-V_step_time = 0.1;
+%% Supply / voltage test
+V_supply   = 6.0;   % Motor supply voltage [V]
+V_step_time = 0.1;  % Voltage applied at [s]
 
-% Initial load
-T_load = 0.1;   % Load torque [N*m]
-T_step_time = 0.5;
+%% Load test
+T_load      = 0.01; % Load torque [N*m]
+T_step_time = 0.5;  % Load applied at [s]
+
+%% Discrete simulation
+Ts_plant = 0.001;    % Plant simulation sample time [s]
